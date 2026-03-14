c91c09945c3d88c377c7a2d6b7ff8e8b45060cb9
Mark Ciril <markciril.jamon@gmail.com>
2026-03-14 15:29:04 +0800

    feat: add interface for task with exptected, std, and variance

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,3 +11,9 @@ export interface Analysis {
     zScore: number
     probability: number
 }
+
+export interface PERTTaskResult extends NewTask {
+    expectedTime: number; // tₑ = (O + 4M + P) / 6
+    standardDeviation: number; // σ  = (P - O) / 6
+    variance: number; // σ²
+}
