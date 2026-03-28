75736585673ee6cb2f2d57de9a019ea78a1afc80
Mark Ciril <markciril.jamon@gmail.com>
2026-03-28 19:04:49 +0800

    feat: add pinned boolean to Project interface

--- src/types/project.ts
+++ src/types/project.ts
@@ -3,6 +3,7 @@ import type { StoredState } from './state'
 export interface Project {
     id: string
     name: string
+    pinned: boolean
     createdAt: string
     updatedAt: string
     state: StoredState
