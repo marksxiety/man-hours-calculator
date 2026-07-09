53955606a3355c1470369bab42654edcc9603a4c
Mark Ciril <markciril.jamon@gmail.com>
2026-07-09 11:09:32 +0800

    feat: add targetDurationEdited flag to StoredState interface

--- src/types/state.ts
+++ src/types/state.ts
@@ -3,6 +3,7 @@ import type { PERTTaskResult } from './task'
 export interface StoredState {
     tasks: PERTTaskResult[]
     targetDuration: number | null
+    targetDurationEdited: boolean
     retainMilestone: boolean
     deleteWarning: boolean
     resetWarning: boolean
